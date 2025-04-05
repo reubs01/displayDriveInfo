f78533b11c3e1fad1a49de6077bf03393b85bf61
Reuben B <112469788+reubs01@users.noreply.github.com>
2025-04-05 23:28:39 +1100

    Update driveInfo.ps1

--- driveInfo.ps1
+++ driveInfo.ps1
@@ -1,12 +1,10 @@
 ﻿# Get all connected drives
 $drives = Get-WmiObject -Class Win32_DiskDrive
 
-# Output header
 Write-Output "----------------------------------------------------"
 Write-Output "                  Drive Information                 "
 Write-Output "----------------------------------------------------"
 
-# Iterate through each drive
 foreach ($drive in $drives) {
     # Get drive information
     $driveID = $drive.DeviceID
@@ -15,7 +13,6 @@ foreach ($drive in $drives) {
     $driveSerial = $drive.SerialNumber
     $driveSize = [math]::Round($drive.Size / 1GB, 2)  # Convert size to GB and round to 2 decimal places
 
-    # Output drive information with formatting
     Write-Output ("Drive ID:        {0,-20}" -f $driveID)
     Write-Output ("Drive Model:     {0,-20}" -f $driveModel)
     Write-Output ("Drive Type:      {0,-20}" -f $driveMediaType)
